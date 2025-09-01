ab732f6413a07ef793db8f92f1c29a0a4b103601
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-09-01 20:10:00 +0900

    管理者画面のダッシュボード修正

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,12 +5,21 @@ class DashboardController extends AppController
 {
     public function index()
     {
-        $this->loadModel('Users');
-        $this->loadModel('Portfolios');
+        $Users = $this->fetchTable('Users');
+        $Companies = $this->fetchTable('Companies');
+        $Portfolios = $this->fetchTable('Portfolios');
+        $Comments = $this->fetchTable('Comments');
 
-        $userCount = $this->Users->find()->count();
-        $portfolioCount = $this->Portfolios->find()->count();
+        $stats = [
+            'users'      => $Users->find()->where(['deleted_at IS' => null])->count(),
+            'companies'  => $Companies->find()->count(),
+            'portfolios' => $Portfolios->find()->count(),
+            'comments'   => $Comments->find()->count(),
+            'pendingCompanies' => $Companies->find()->where(['verified !=' => 1])->count(),
+            'privatePortfolios' => $Portfolios->find()->where(['is_public' => 0])->count(),
+        ];
 
-        $this->set(compact('userCount', 'portfolioCount'));
+        $this->set(compact('stats'));
+        $this->viewBuilder()->setOption('serialize', []);
     }
 }
