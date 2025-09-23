567b4aef0bc2a160d7888789dc10549d9a386b06
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-09-23 19:16:38 +0900

    テストコントローラー追加（これいるか？）

---
+++ tests/TestCase/Controller/Employer/SettingsControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\/Employer/SettingsController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\/Employer/SettingsController Test Case
+ *
+ * @uses \App\Controller\/Employer/SettingsController
+ */
+class /Employer/SettingsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+}
