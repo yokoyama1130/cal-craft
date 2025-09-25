9ef5014155cf5772b98c0108ee81f543913944c4
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-09-25 13:20:35 +0900

    ドックブロックコメントを追加

--- src/Controller/Admin/AppController.php
+++ src/Controller/Admin/AppController.php
@@ -7,6 +7,15 @@ use App\Controller\AppController as BaseController;
 
 class AppController extends BaseController
 {
+    /**
+     * アクション実行前の共通処理（管理者チェック）。
+     *
+     * - 認証情報を取得し、管理者メールアドレス以外のユーザーはトップページへリダイレクト
+     * - 管理者ページ用のレイアウトを適用
+     *
+     * @param \Cake\Event\EventInterface $event イベントオブジェクト
+     * @return \Cake\Http\Response|null リダイレクトレスポンス または null
+     */
     public function beforeFilter(\Cake\Event\EventInterface $event)
     {
         parent::beforeFilter($event);
@@ -15,6 +24,7 @@ class AppController extends BaseController
 
         if (!$identity || $identity->get('email') !== 'yokoyama.ryogo.1130@gmail.com') {
             $this->Flash->error('管理者専用ページです');
+
             return $this->redirect('/');
         }
 
