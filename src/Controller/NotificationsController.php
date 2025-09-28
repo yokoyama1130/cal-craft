edfde939fa405d5fa45add314f6c3a671f9221b0
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-09-28 13:54:35 +0900

    通知コントローラーリファクタリング（後で会社アカウントの通知も来るように修正する必要アリかも）

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -5,25 +5,32 @@ namespace App\Controller;
 
 class NotificationsController extends AppController
 {
+    /**
+     * 通知一覧の表示
+     *
+     * - ログイン中ユーザーの通知を取得し、最新順に一覧表示
+     * - 一覧画面に遷移したタイミングで未読通知を既読状態に更新
+     *
+     * @return void
+     */
     public function index()
     {
-        $this->loadModel('Notifications');
-    
+        $this->Notifications = $this->fetchTable('Notifications');
+
         $userId = $this->request->getAttribute('identity')->get('id');
-    
+
         $notifications = $this->Notifications->find()
             ->where(['user_id' => $userId])
             ->contain(['SenderUsers']) // ← ここ追加
             ->order(['created' => 'DESC'])
             ->toArray();
-    
+
         $this->set(compact('notifications'));
-    
+
         // 一覧を表示したら未読を既読に更新
         $this->Notifications->updateAll(
             ['is_read' => true],
             ['user_id' => $userId, 'is_read' => false]
         );
     }
-    
 }
