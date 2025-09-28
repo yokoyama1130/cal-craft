6fd1ee35ee1d24625ce8fcdcb8270d603f9b105e
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-09-28 14:23:36 +0900

    トップコントローラーリファクタリング完了

--- src/Controller/TopController.php
+++ src/Controller/TopController.php
@@ -7,8 +7,15 @@ use Cake\Event\EventInterface;
 
 class TopController extends AppController
 {
-    // src/Controller/PortfoliosController.php
-
+    /**
+     * beforeFilter
+     *
+     * 各アクション実行前のフィルタ処理。
+     * TopController では index アクションだけ未ログイン状態でもアクセス可能に設定する。
+     *
+     * @param \Cake\Event\EventInterface $event イベントオブジェクト
+     * @return void
+     */
     public function beforeFilter(EventInterface $event)
     {
         parent::beforeFilter($event);
@@ -17,46 +24,54 @@ class TopController extends AppController
         $this->Authentication->addUnauthenticatedActions(['index']);
     }
 
+    /**
+     * index
+     *
+     * 公開されている最新のポートフォリオを最大10件まで取得し、トップページに表示する。
+     * 各ポートフォリオについて「いいね数」と「自分がいいね済みかどうか」を付与する。
+     * ユーザー/会社アカウントいずれのログインにも対応。
+     *
+     * @return void
+     */
     public function index()
     {
-        $this->loadModel('Likes');
-        $this->loadModel('Portfolios');
-    
+        $this->Likes = $this->fetchTable('Likes');
+        $this->Portfolios = $this->fetchTable('Portfolios');
+
         $identity = $this->request->getAttribute('identity');
         $actor = [];
         if ($identity) {
             $id = (int)$identity->get('id');
             // Users or Companies?
-            $this->loadModel('Users');
+            $this->Users = $this->fetchTable('Users');
             if ($this->Users->exists(['id' => $id])) {
                 $actor = ['user_id' => $id];
             } else {
-                $this->loadModel('Companies');
+                $this->Companies = $this->fetchTable('Companies');
                 if ($this->Companies->exists(['id' => $id])) {
                     $actor = ['company_id' => $id];
                 }
             }
         }
-    
+
         $portfolios = $this->Portfolios->find()
             ->contain(['Users'])
             ->where(['is_public' => true])
             ->order(['created' => 'DESC'])
             ->limit(10)
             ->toArray();
-    
+
         foreach ($portfolios as $p) {
             $p->like_count = $this->Likes->find()
                 ->where(['portfolio_id' => $p->id])
                 ->count();
-    
+
             $p->liked_by_me = false;
             if ($actor) {
                 $p->liked_by_me = $this->Likes->exists(array_merge(['portfolio_id' => $p->id], $actor));
             }
         }
-    
-        $this->set(compact('portfolios'));
-    }    
 
+        $this->set(compact('portfolios'));
+    }
 }
