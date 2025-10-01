2cab4914878a3429806e76994161676f6f0548bd
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-01 16:05:42 +0900

    無駄な空白の削除

--- src/Model/Table/CategoriesTable.php
+++ src/Model/Table/CategoriesTable.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
-use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
@@ -12,7 +10,6 @@ use Cake\Validation\Validator;
  * Categories Model
  *
  * @property \App\Model\Table\PortfoliosTable&\Cake\ORM\Association\HasMany $Portfolios
- *
  * @method \App\Model\Entity\Category newEmptyEntity()
  * @method \App\Model\Entity\Category newEntity(array $data, array $options = [])
  * @method \App\Model\Entity\Category[] newEntities(array $data, array $options = [])
