e10ac211e16010c8fd310a977ea41cff41ceca0a
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-02 08:48:35 +0900

    ダッシュボードのビューのエラー修正

--- templates/Admin/Dashboard/index.php
+++ templates/Admin/Dashboard/index.php
@@ -1,16 +1,16 @@
-<?php $this->assign('title','ダッシュボード') ?>
+<?php $this->assign('title', 'ダッシュボード') ?>
 <div class="row g-3">
   <?php
     $cards = [
-      ['title'=>'ユーザー','val'=>$stats['users'],'icon'=>'fa-user'],
-      ['title'=>'企業','val'=>$stats['companies'],'icon'=>'fa-building'],
-      ['title'=>'ポートフォリオ','val'=>$stats['portfolios'],'icon'=>'fa-images'],
-      ['title'=>'コメント','val'=>$stats['comments'],'icon'=>'fa-comments'],
-      ['title'=>'未認証企業','val'=>$stats['pendingCompanies'],'icon'=>'fa-shield-halved'],
-      ['title'=>'非公開PF','val'=>$stats['privatePortfolios'],'icon'=>'fa-lock'],
+      ['title' => 'ユーザー', 'val' => $stats['users'], 'icon' => 'fa-user'],
+      ['title' => '企業', 'val' => $stats['companies'], 'icon' => 'fa-building'],
+      ['title' => 'ポートフォリオ', 'val' => $stats['portfolios'], 'icon' => 'fa-images'],
+      ['title' => 'コメント', 'val' => $stats['comments'], 'icon' => 'fa-comments'],
+      ['title' => '未認証企業', 'val' => $stats['pendingCompanies'], 'icon' => 'fa-shield-halved'],
+      ['title' => '非公開PF', 'val' => $stats['privatePortfolios'], 'icon' => 'fa-lock'],
     ];
-  ?>
-  <?php foreach($cards as $c): ?>
+    ?>
+  <?php foreach ($cards as $c) : ?>
     <div class="col-12 col-sm-6 col-lg-4">
       <div class="card p-3">
         <div class="d-flex align-items-center justify-content-between">
