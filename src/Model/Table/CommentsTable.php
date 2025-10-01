eeea63fa3d0a02ca8e8a7c2fc8b67e935f3c08bf
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-01 16:07:44 +0900

    ドッグブロックコメントの追加

--- src/Model/Table/CommentsTable.php
+++ src/Model/Table/CommentsTable.php
@@ -3,17 +3,14 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
-use Cake\Validation\Validator;
 
 /**
  * Comments Model
  *
  * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsTo $Users
  * @property \App\Model\Table\PortfoliosTable&\Cake\ORM\Association\BelongsTo $Portfolios
- *
  * @method \App\Model\Entity\Comment newEmptyEntity()
  * @method \App\Model\Entity\Comment newEntity(array $data, array $options = [])
  * @method \App\Model\Entity\Comment[] newEntities(array $data, array $options = [])
@@ -27,7 +24,6 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\Comment[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
  * @method \App\Model\Entity\Comment[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
  * @method \App\Model\Entity\Comment[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
- *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
  */
 class CommentsTable extends Table
@@ -50,12 +46,12 @@ class CommentsTable extends Table
 
         $this->belongsTo('Users', [
             'foreignKey' => 'user_id',
-            'joinType'   => 'LEFT',   // 会社コメント時は NULL なので LEFT
+            'joinType' => 'LEFT', // 会社コメント時は NULL なので LEFT
         ]);
-        
+
         $this->belongsTo('Companies', [
             'foreignKey' => 'company_id',
-            'joinType'   => 'LEFT',
+            'joinType' => 'LEFT',
         ]);
 
         $this->belongsTo('Portfolios', [
@@ -67,28 +63,29 @@ class CommentsTable extends Table
     /**
      * Default validation rules.
      *
-     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @param \Cake\Validation\Validator $v Validator instance.
      * @return \Cake\Validation\Validator
      */
     public function validationDefault(\Cake\Validation\Validator $v): \Cake\Validation\Validator
     {
         $v->integer('portfolio_id')->notEmptyString('portfolio_id');
         $v->notEmptyString('content');
-    
+
         // user/company はどちらか片方だけ必須（アプリ側で担保）
         $v->allowEmptyString('user_id');
         $v->allowEmptyString('company_id');
-    
+
         // カスタムルール：「user_id XOR company_id」
         $v->add('user_id', 'xorUserCompany', [
             'rule' => function ($value, $context) {
                 $u = $value ?? null;
                 $c = $context['data']['company_id'] ?? null;
+
                 return ($u && !$c) || (!$u && $c);
             },
             'message' => 'ユーザーまたは会社のどちらか一方を指定してください。',
         ]);
-    
+
         return $v;
     }
 
