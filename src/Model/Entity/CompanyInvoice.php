1694eb40a80f4f8c549441173d2038764bd5a546
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-01 16:00:32 +0900

    無駄な空白を削除

--- src/Model/Entity/CompanyInvoice.php
+++ src/Model/Entity/CompanyInvoice.php
@@ -8,19 +8,19 @@ use Cake\ORM\Entity;
 class CompanyInvoice extends Entity
 {
     protected $_accessible = [
-        'company_id'               => true,
-        'stripe_customer_id'       => true,
-        'stripe_subscription_id'   => true,
-        'stripe_invoice_id'        => true,
+        'company_id' => true,
+        'stripe_customer_id' => true,
+        'stripe_subscription_id' => true,
+        'stripe_invoice_id' => true,
         'stripe_payment_intent_id' => true,
-        'plan'                     => true,
-        'amount'                   => true,
-        'currency'                 => true,
-        'status'                   => true,
-        'paid_at'                  => true,
-        'raw_payload'              => true,
-        'created'                  => true,
-        'modified'                 => true,
-        'company'                  => true,
+        'plan' => true,
+        'amount' => true,
+        'currency' => true,
+        'status' => true,
+        'paid_at' => true,
+        'raw_payload' => true,
+        'created' => true,
+        'modified' => true,
+        'company' => true,
     ];
 }
