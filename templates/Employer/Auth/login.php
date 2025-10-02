fcd68d103dd8a124ea67512ee592f9859c719153
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-02 11:08:26 +0900

    必要なカンマを追加

--- templates/Employer/Auth/login.php
+++ templates/Employer/Auth/login.php
@@ -14,21 +14,21 @@
     <?= $this->Form->create(null, [
         // ここを controller 付きにしない
         'url' => '/employer/login',
-        'autocomplete' => 'off'
+        'autocomplete' => 'off',
     ]) ?>
 
   <?= $this->Form->control('auth_email', [
     'label' => 'Email',
     'type' => 'email',
     'required' => true,
-    'class' => 'form-control'
+    'class' => 'form-control',
   ]) ?>
 
   <?= $this->Form->control('auth_password', [
     'label' => 'Password',
     'type' => 'password',
     'required' => true,
-    'class' => 'form-control'
+    'class' => 'form-control',
   ]) ?>
 
   <div class="mt-3">
@@ -39,7 +39,7 @@
   <p class="mt-3">
     会員登録がまだの方は
     <?= $this->Html->link('こちら', [
-        'prefix' => false,           // ← これが大事
+        'prefix' => false,
         'controller' => 'Companies',
         'action' => 'add',
     ]) ?>
