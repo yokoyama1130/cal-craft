f0e5ca6eb883186d41b22c8bb8b1b45624ccef09
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-02 11:07:37 +0900

    必要なカンマを追加

--- templates/element/pagination.php
+++ templates/element/pagination.php
@@ -4,7 +4,7 @@
  * PaginatorHelper 前提
  */
 ?>
-<?php if ($this->Paginator->total() > 1): ?>
+<?php if ($this->Paginator->total() > 1) : ?>
 <nav aria-label="Pagination">
     <ul class="pagination">
         <li class="page-item"><?= $this->Paginator->first('«', ['escape' => false, 'class' => 'page-link']) ?></li>
@@ -16,7 +16,7 @@
             'templates' => [
                 'number' => '<a class="page-link" href="{{url}}">{{text}}</a>',
                 'current' => '<span class="page-link active">{{text}}</span>',
-            ]
+            ],
         ]) ?>
         <li class="page-item"><?= $this->Paginator->next('›', ['escape' => false, 'class' => 'page-link']) ?></li>
         <li class="page-item"><?= $this->Paginator->last('»', ['escape' => false, 'class' => 'page-link']) ?></li>
