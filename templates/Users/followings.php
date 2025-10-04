56beab71d1af278c3afd895f3f947a86c46d1077
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-04 17:21:15 +0900

    フォロー欄修正（しょぼすぎるから後で修正

--- templates/Users/followings.php
+++ templates/Users/followings.php
@@ -1,6 +1,6 @@
 <h2>フォロー一覧</h2>
 <ul class="list-group">
-    <?php foreach ($followings as $f): ?>
+    <?php foreach ($followings as $f) : ?>
         <li class="list-group-item">
             <?= $this->Html->link(
                 h($f->followed_user->name),
